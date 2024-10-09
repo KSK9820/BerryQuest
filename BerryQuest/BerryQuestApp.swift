2eedca48556e21d5dbd42906bc2d9f7bae2fa2a5
KSK9820 <68066104+KSK9820@users.noreply.github.com>
2024-10-09 17:37:04 +0900

    feat: 프로젝트 초기 세팅

---
+++ BerryQuest/BerryQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BerryQuestApp.swift
+//  BerryQuest
+//
+//  Created by 김수경 on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct BerryQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
